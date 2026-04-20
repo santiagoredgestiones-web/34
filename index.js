54e0c7b41dccb29585e5812f89cdac8b630f7769
santiagoredgestiones-web <santiagoredgestiones@gmail.com>
2026-04-20 20:27:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Backend Ruta 34 funcionando 🚀");
-});
-
+const cors = require("cors");
 const { obtenerNoticias } = require("./scraper");
-
-app.get("/noticias", async (req, res) => {
-  const noticias = await obtenerNoticias();
-  res.json(noticias);
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Servidor corriendo"));
